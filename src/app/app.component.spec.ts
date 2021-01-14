efeb8ade5f3c674aceae3bb495ddeb488ec6484d
Abdelfattah Ragab <mscaragab@gmail.com>
2021-01-14 22:50:26 +0200

    'Update'

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'lemonada'`, () => {
+  it(`should have as title 'cleanux'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('lemonada');
+    expect(app.title).toEqual('cleanux');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('lemonada app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('cleanux app is running!');
   });
 });
